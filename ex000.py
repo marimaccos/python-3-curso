10aaaa4af93782525d3f4a7f20479ef699fb02e4
Mariana Machado <mariana_maccos@hotmail.com>
2021-10-26 12:23:11 -0300

    Adicionando primeiros 5 exercícios do curso

---
+++ ex000.py
@@ -0,0 +1,6 @@
+# ler arquivo txt
+
+arq = open('textoteste.txt', 'r')
+texto = arq.read()
+
+print(texto)
---
+++ ex002.py
@@ -0,0 +1,4 @@
+nome = input('Qual o seu nome? ')
+
+print("Bem-vindo, {}!".format(nome))
+
